68435ccb70351ae2482911f981d74789e3a10b1d
Ameen <98791784+kpameen@users.noreply.github.com>
2025-05-16 02:06:34 +0530

    Update entire site with new code

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ AOS.init({
   duration: 1000,
   once: true
 });
+
+window.addEventListener('scroll', function () {
+  const header = document.getElementById('main-header');
+  if (window.scrollY > 50) {
+    header.classList.add('shrink');
+  } else {
+    header.classList.remove('shrink');
+  }
+});
+
+function toggleMenu() {
+  const nav = document.getElementById('navbar');
+  nav.classList.toggle('active');
+}
+
